98f71a4283c37208746752764431b98e6c1ba9c5
Eldar2021 <eldiiaralmazbekov@gmail.com>
2023-07-14 00:50:06 +0600

    add counter api

---
+++ counter/routes/index.dart
@@ -0,0 +1,6 @@
+import 'package:dart_frog/dart_frog.dart';
+
+Response onRequest(RequestContext context) {
+  final count = context.read<int>();
+  return Response(body: 'You have requested this route $count time(s).');
+}
